3cfbb279db063d12480954a873c2a8254f2f2f7f
Chao Sun <83864455+chaosun-ai@users.noreply.github.com>
2023-11-09 00:02:27 +0000

    add classifier chain

--- AutoGPT.py
+++ AutoGPT.py
@@ -2,14 +2,52 @@ from dotenv import load_dotenv
 #from langchain.llms import OpenAI
 from langchain.chat_models import AzureChatOpenAI
 from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.output_parsers.openai_functions import PydanticAttrOutputFunctionsParser
+from langchain.utils.openai_functions import convert_pydantic_to_openai_function
+from operator import itemgetter
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.runnable import RunnableBranch
+from typing import Literal
+
+from collections import deque
+
+from utility import TopicClassifier
 import streamlit as st
 
 # Load env vars
 load_dotenv()
 
-st.title('AutoGPT Wizard')
-prompt = st.text_input('Tell me a topic you want to learn its programming language:') 
+st.title('Woman Cloth Helper Wizard')
+prompt = st.text_input('Tell me what woman cloth would you like me to recommend.') 
+
+
+
+# Define the prompt
+fashion_template = """You're a very knowledgeable woman cloth fashion expert \
+    who provides accurate and eloquent answers to fashion-related questions and recommend cloth for customers. \
+    When you don't know the answer to a question you admit that you don't know.
+    
+    Here is a question:
+    {input}"""
+fashion_prompt = PromptTemplate.from_template(fashion_template)
+
+#classifier_template =     """You're a very knowledgeable woman cloth fashion expert \
+#    who provides accurate and eloquent answers to fashion-related questions and recommend cloth for customers. \
+#    if the question is related to your expertise, then reply 'related'. Otherwise, reply 'not_related'."""
+
+classify_type = Literal["fashion", "cloth searching", "general"]
+classifier_template_str = "Please classify user question {input} and reply either 'fashion', 'cloth searching' or 'other' "
+
+
+classifier_prompt = PromptTemplate(
+    template=classifier_template_str,
+    input_variables=['input'],
+    
+)
+
 
 # Prompt templates
 language_template = PromptTemplate(
@@ -25,11 +63,26 @@ book_recommendation_template = PromptTemplate(
     '''
 )
 
-#llm = OpenAI(temperature=0.9, model_name="gpt-3.5-turbo") 
 llm = AzureChatOpenAI(
+    temperature=0,
     deployment_name="azure-gpt-35-turbo",
     model_name="azure-text-embedding-ada-002"
 )
+
+# Define classifier chains
+classifier_function = convert_pydantic_to_openai_function(TopicClassifier)
+llm_classifier = llm.bind(
+    functions=[classifier_function], function_call={"name": "TopicClassifier"}
+)
+parser = PydanticAttrOutputFunctionsParser(
+    pydantic_schema=TopicClassifier, attr_name="topic"
+)
+classifier_chain = llm_classifier | parser
+
+# default chain
+default_chain = "I can only answer questions about fashion."
+
+classifier_chain = LLMChain(llm=llm, prompt=classifier_prompt, verbose=True, output_key='classification_result')
 language_chain = LLMChain(llm=llm, prompt=language_template, verbose=True, output_key='programming_language')
 book_recommendation_chain = LLMChain(llm=llm, prompt=book_recommendation_template, verbose=True, output_key='book_name')
 
@@ -39,14 +92,23 @@ sequential_chain = SequentialChain(
     output_variables=['programming_language', 'book_name'],
     verbose=True)
 
+prompt_reply_queue = deque(maxlen=10)
+
+
 if prompt: 
-    reply = sequential_chain({'topic': prompt})
+    reply = classifier_chain({'input': prompt})
+    if 
 
-    with st.expander("Result"):
-        st.info(reply)
+
+
+    prompt_reply_queue.append(prompt)
+    prompt_reply_queue.append(reply['classification_result'])
+
+    st.text_area("Answer", value=reply['classification_result'])
         
-    with st.expander("Programming Language"):
-        st.info(reply['programming_language'])
+    strings = "\n".join(prompt_reply_queue)
+    with st.expander("Chat History"):
+        st.write(strings)
 
-    with st.expander("Recommended Book"):
-        st.info(reply['book_name'])
+    #with st.expander("Recommended Book"):
+    #    st.info(reply['book_name'])
